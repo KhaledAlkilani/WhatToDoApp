ba55d21ba15348d9deed34b57ec34b6f69b1cf8a
Khaled Alkilani <85066014+KhaledAlkilani@users.noreply.github.com>
2025-01-21 11:33:55 +0200

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Dashboard from "./routes/Dashboard";
 import Tasks from "./components/Tasks/Tasks";
 import ErrorPage from "./components/ErrorPage";
 
+// just to make a pull request for this branch
+
 export default function App() {
   return (
     <BrowserRouter>
